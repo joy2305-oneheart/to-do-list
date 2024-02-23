4384c1bdd8388554c0ea0888fceff841bf0b5cd0
Henry.abhinav <henryabhinav@rubicotech.in>
2024-02-23 15:34:22 +0530

    worked on to-do list

--- index .js
+++ index .js
@@ -1,9 +1,33 @@
-function addUlAndLi() {
-    var ul = document.createElement("ul");
-    var li = document.createElement("li");
-    li.textContent = "New Item";
-    ul.appendChild(li);
-    document.body.appendChild(ul);
+const inputBox = document.getElementById("input-text");
+let btn = document.getElementById("addTask");
+const listRow = document.querySelector(".to-do-list__row");
+
+let newUl = "";
+
+function createList() {
+    if(newUl === "") {
+        newUl =  document.createElement("ul");
+        newUl.classList.add('list-group', 'mt-5');
+        listRow.appendChild(newUl);
+    }
+    let li = document.createElement("li"); 
+    li.textContent = inputBox.value;
+    newUl.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.classList.add('list-group-item');
+    li.appendChild(span);
+    inputBox.value= '';
 }
-var addButton = document.getElementById("addButton");
-addButton.addEventListener("click", addUlAndLi);
+
+let addTask = () => (inputBox.value === "") ? alert("Make a List") : createList();
+btn.addEventListener("click", addTask);
+
+ul.addEventListener("click", function(e){
+    if(e.target.tagName ==="li"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
